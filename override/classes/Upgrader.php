bfc7dc1ef564eff37f4be53cdf04e114264d99c8
Michael <github@marinetti.fr>
2012-02-29 16:08:32 +0100

    dev version bis

--- override/classes/Upgrader.php
+++ override/classes/Upgrader.php
@@ -2,7 +2,20 @@
 
 class Upgrader extends UpgraderCore
 {
-	// common channel
-	public $rss_version_link = 'http://www.prestashop.com/xml/upgrader.xml';
+	/**
+	 * link to xml which contains urls for downloading 
+	 * (default is http://api.prestashop.com/xml/upgrader.xml ) 
+	 * 
+	 * @var string
+	 */
+	public $rss_version_link = 'http://prestadev.marinetti.fr/xml/upgrader.xml';
+	/**
+	 * root directory which contains all [prestashop version].xml  
+	 * (default is http://api.prestashop.com/xml/md5/ ) 
+	 * 
+	 * @var string
+	 */
+	public $rss_md5file_link_dir = 'http://prestadev.marinetti.fr/xml/md5/';
+
 }
 
